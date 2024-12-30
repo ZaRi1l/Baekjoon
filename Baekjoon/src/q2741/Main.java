3173178d8ac6b9b82d913fe2e3593cd978910ab6
ZaRi1l <zari_09@naver.com>
2024-12-30 14:32:32 +0900

    2741 commit

--- Baekjoon/src/q2741/Main.java
+++ Baekjoon/src/q2741/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int tmp = sc.nextInt();
+		int N = sc.nextInt();
 		
-		for(int i = 0; i <= tmp; i++) {
+		for(int i = 1; i <= N; i++) {
 			System.out.println(i);
 		}
+		
+		sc.close();
 	}
 
 }
