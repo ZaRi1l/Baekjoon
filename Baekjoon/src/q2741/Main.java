b91b1a5a5b9df2e10c6106f6fcab275987debbcf
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2024-12-30 14:16:19 +0900

    initial commit

---
+++ Baekjoon/src/q2741/Main.java
@@ -0,0 +1,16 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tmp = sc.nextInt();
+		
+		for(int i = 0; i <= tmp; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
