7606f2fe7cd319881f0b43933d123cf9e946de92
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2024-12-30 16:51:45 +0900

    q9498-2 commit

--- Baekjoon/src/q9498/Main.java
+++ Baekjoon/src/q9498/Main.java
@@ -9,17 +9,38 @@ public class Main {
 		int score = sc.nextInt();
 		sc.close();
 		
-		if (90 <= score && score <= 100) {
+		
+		switch(score / 10) {
+		case 10:
+		case 9:
 			System.out.println('A');
-		} else if (80 <= score && score <= 89) {
+			break;
+		case 8:
 			System.out.println('B');
-		} else if (70 <= score && score <= 79) {
+			break;
+		case 7:
 			System.out.println('C');
-		} else if (60 <= score && score <= 69) {
+			break;
+		case 6:
 			System.out.println('D');
-		} else {
-			System.out.println('F');
+			break;
+		default:
+			System.out.println('F');	
+			break;
 		}
+		
+		
+//		if (90 <= score && score <= 100) {
+//			System.out.println('A');
+//		} else if (80 <= score && score <= 89) {
+//			System.out.println('B');
+//		} else if (70 <= score && score <= 79) {
+//			System.out.println('C');
+//		} else if (60 <= score && score <= 69) {
+//			System.out.println('D');
+//		} else {
+//			System.out.println('F');
+//		}
 	}
 
 }
