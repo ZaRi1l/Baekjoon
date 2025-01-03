3d51f22b90857667abf0d8ec21f5affc49d048a2
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-04 04:15:09 +0900

    q10871 commit

---
+++ Baekjoon/src/q10871/Main.java
@@ -0,0 +1,22 @@
+package q10871;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] seq = new int[sc.nextInt()];
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < seq.length; i++) {
+			seq[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < seq.length; i++) {
+			if (seq[i] < n) System.out.printf("%d ", seq[i]);
+		}
+		
+	}
+
+}
