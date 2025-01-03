8e0353bfa59fb76121853ff905a0fd29b54bf87b
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-04 04:26:31 +0900

    q3046

---
+++ Baekjoon/src/q3046/Main.java
@@ -0,0 +1,13 @@
+package q3046;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(-(sc.nextInt() - sc.nextInt() * 2));
+		sc.close();
+	}
+
+}
