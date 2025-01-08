7ffcfcf7c16eebbdb3dea73f37048d3da12ee957
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-08 17:42:32 +0900

    bigO commit

---
+++ Baekjoon/src/q24262/Main.java
@@ -0,0 +1,15 @@
+package q24262;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(n);
+		System.out.println(1);
+	}
+
+}
