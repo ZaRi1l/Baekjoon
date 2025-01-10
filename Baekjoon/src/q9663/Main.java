af188333b7d5407d558c7806478d1a4958fe1a7d
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-10 17:23:00 +0900

    q9663 try 2

--- Baekjoon/src/q9663/Main.java
+++ Baekjoon/src/q9663/Main.java
@@ -5,82 +5,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int cs = 0;
+	static int cs = 0;		// 경우의 수 더하기
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		br.close();
 
-		int[][] chess = new int[n][n];
-		int[][] chessBackUp = new int[n][n];
+		int[][] chess = new int[n][n];			// chess 판
 
-		for (int i = 0; i < chess.length; i++) {
-			for (int j = 0; j < chess[i].length; j++) {
-				System.out.print(chess[i][j] + " ");
-			}
-			System.out.println();
-		}
+//		for (int i = 0; i < chess.length; i++) {			// 출력 해보기
+//			for (int j = 0; j < chess[i].length; j++) {
+//				System.out.print(chess[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
 
-		for (int i = 0; i < chess.length; i++) {
+		for (int i = 0; i < chess.length; i++) {	// 1행의 모든 수를 대입해보기
 			backTrack(chess, 0, i);
 			chess = new int[n][n];
 		}
 
-		for (int i = 0; i < chess.length; i++) {
-			for (int j = 0; j < chess[i].length; j++) {
-				System.out.print(chess[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println(cs);
-
+//		for (int i = 0; i < chess.length; i++) {		// 출력해보기
+//			for (int j = 0; j < chess[i].length; j++) {
+//				System.out.print(chess[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		System.out.println(cs);				// 경우의 수 출력
+	
 	}
 
-	static boolean backTrack(int[][] chess, int x, int y) {
-		if (chess[x][y] == 1)
-			return false;
-		if (chess[x][y] == 0 && x == chess.length - 1) {
+	static void backTrack(int[][] chess, int x, int y) {
+		if (chess[x][y] == 1)			// 차 있으면 끝탬
+			return;
+		if (chess[x][y] == 0 && x == chess.length - 1) {		// 안 차있고, 마지막 행이면 경우의 수 cs에 1 더하기
 			cs += 1;
-			return true;
+			return;
 		}
 
-		int[][] chessBackUp = chess.clone();
-		for (int i = 0; i < chess.length; i++) {
-			chessBackUp[i] = chess[i].clone();
-		}
 
-		for (int i = 0; i < chess.length; i++) {
+		for (int i = 0; i < chess.length; i++) {				// 해당 좌표에서 퀸이 갈수 있는 범위에 1 채우기
 			chess[x][i] = 1;
 			chess[i][y] = 1;
 
 			if (x + i < chess.length && y + i < chess.length)
 				chess[x + i][y + i] = 1;
 
-//			if (x - i >= 0 && y - i >= 0)
-//				chess[x - i][y - i] = 1;
-
 			if (x + i < chess.length && y - i >= 0)
 				chess[x + i][y - i] = 1;
-
-//			if (x - i >= 0 && y + i < chess.length)
-//				chess[x - i][y + i] = 1;
+		}
+		
+		
+		int[][] chessBackUp = chess.clone();				// 백업용 복사본 만들기
+		for (int i = 0; i < chess.length; i++) {
+			chessBackUp[i] = chess[i].clone();
 		}
 
 		boolean success = false;
 		for (int j = 0; j < chess.length; j++) {
 			if (chess[x + 1][j] != 1) {
-				if (backTrack(chess, x + 1, j)) {
-					success = true;
-				} else {
-					for (int i = 0; i < chess.length; i++) {
-						chess[i] = chessBackUp[i].clone();
-					}
+				backTrack(chess, x + 1, j);					// 다음행으로 넘어가서 확인하기
+				for (int i = 0; i < chess.length; i++) {	// 다음행 가기 전으로 초기화 하기
+					chess[i] = chessBackUp[i].clone();
 				}
 			}
 		}
-
-		return success;
 	}
-
 }
