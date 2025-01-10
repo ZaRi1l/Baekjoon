91a62e30991e40ee69b2f2300961fb18f82a5085
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-10 17:40:37 +0900

    q9663 try 3

--- Baekjoon/src/q9663/Main.java
+++ Baekjoon/src/q9663/Main.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int cs = 0;		// 경우의 수 더하기
+	static int cs = 0; // 경우의 수 더하기
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		br.close();
 
-		int[][] chess = new int[n][n];			// chess 판
+		int[][] chess = new int[n][n]; // chess 판
 
 //		for (int i = 0; i < chess.length; i++) {			// 출력 해보기
 //			for (int j = 0; j < chess[i].length; j++) {
@@ -21,7 +21,7 @@ public class Main {
 //			System.out.println();
 //		}
 
-		for (int i = 0; i < chess.length; i++) {	// 1행의 모든 수를 대입해보기
+		for (int i = 0; i < chess.length; i++) { // 1행의 모든 수를 대입해보기
 			backTrack(chess, 0, i);
 			chess = new int[n][n];
 		}
@@ -32,20 +32,19 @@ public class Main {
 //			}
 //			System.out.println();
 //		}
-		System.out.println(cs);				// 경우의 수 출력
-	
+		System.out.println(cs); // 경우의 수 출력
+
 	}
 
 	static void backTrack(int[][] chess, int x, int y) {
-		if (chess[x][y] == 1)			// 차 있으면 끝탬
+		if (chess[x][y] == 1) // 차 있으면 끝탬
 			return;
-		if (chess[x][y] == 0 && x == chess.length - 1) {		// 안 차있고, 마지막 행이면 경우의 수 cs에 1 더하기
+		if (chess[x][y] == 0 && x == chess.length - 1) { // 안 차있고, 마지막 행이면 경우의 수 cs에 1 더하기
 			cs += 1;
 			return;
 		}
 
-
-		for (int i = 0; i < chess.length; i++) {				// 해당 좌표에서 퀸이 갈수 있는 범위에 1 채우기
+		for (int i = 0; i < chess.length; i++) { // 해당 좌표에서 퀸이 갈수 있는 범위에 1 채우기
 			chess[x][i] = 1;
 			chess[i][y] = 1;
 
@@ -55,19 +54,17 @@ public class Main {
 			if (x + i < chess.length && y - i >= 0)
 				chess[x + i][y - i] = 1;
 		}
-		
-		
-		int[][] chessBackUp = chess.clone();				// 백업용 복사본 만들기
-		for (int i = 0; i < chess.length; i++) {
-			chessBackUp[i] = chess[i].clone();
+
+		int[][] chessBackUp = new int[chess.length - x][]; // 백업용 복사본 만들기	 행 아랫부분만 저장할 것
+		for (int i = 0; i < chessBackUp.length; i++) {
+			chessBackUp[i] = chess[i + x].clone();
 		}
 
-		boolean success = false;
 		for (int j = 0; j < chess.length; j++) {
 			if (chess[x + 1][j] != 1) {
-				backTrack(chess, x + 1, j);					// 다음행으로 넘어가서 확인하기
-				for (int i = 0; i < chess.length; i++) {	// 다음행 가기 전으로 초기화 하기
-					chess[i] = chessBackUp[i].clone();
+				backTrack(chess, x + 1, j); // 다음행으로 넘어가서 확인하기
+				for (int i = 0; i < chessBackUp.length; i++) { // 다음행 가기 전으로 초기화 하기
+					chess[i + x] = chessBackUp[i].clone();
 				}
 			}
 		}
