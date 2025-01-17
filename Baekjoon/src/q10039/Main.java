3e59a534b8f01c0614fba40bd4e15989b84ee571
ZaRi1l <zari_09@naver.com>
2025-01-17 10:40:09 +0900

    test

--- Baekjoon/src/q10039/Main.java
+++ Baekjoon/src/q10039/Main.java
@@ -7,14 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int sum = 0;
-		
+
 		for (int i = 0; i < 5; i++) {
 			int score = sc.nextInt();
 			sum += (score < 40) ? 40 : score;
 		}
 		sc.close();
-		
-		System.out.print(sum/5);
+
+		System.out.print(sum / 5);
 	}
 
 }
