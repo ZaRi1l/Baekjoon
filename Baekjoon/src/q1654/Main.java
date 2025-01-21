5855b74aff9c1799d48545bcd35bec451729c54b
ZaRi1l <zari_09@naver.com>
2025-01-21 17:27:10 +0900

    q1654 try

--- Baekjoon/src/q1654/Main.java
+++ Baekjoon/src/q1654/Main.java
@@ -11,29 +11,53 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int k = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
-		
+
 		int[] arr = new int[k];
-		
-		for(int i = 0 ; i < arr.length; i++) 
+		int[] arr2;
+
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-		
-		Arrays.sort(arr);
-		
-		int sum = 0;
-		for(int i = 0 ; i < arr.length; i++) {
-			System.out.println(arr[i]);
-			sum += arr[i];
 		}
-		System.out.println(sum);
-		
+
+		Arrays.sort(arr);
+		arr2 = arr.clone();
+
+		int idx = arr2.length - 1;
+		int cm = 0;
+		while (true) {
+			int count = 0;
+
+			cm = arr2[idx];
+			for (int i = 0; i < arr.length; i++) {
+				count += arr[i] / cm;
+			}
+
+			if (count >= n)
+				break;
+
+			arr2[idx] = arr2[idx] / 2;
+			if (idx == 0) {
+				idx = arr2.length - 1;
+			} else {
+				idx -= 1;
+			}
+		}
+
+		System.out.println(cm);
+
+//		for(int i = 0 ; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//			System.out.println(arr2[i]);
+//		}
+
 //		int max = arr[arr.length - 1];
 //		while(true) {
 //			
 //		}
-	
+
 	}
 
 }
