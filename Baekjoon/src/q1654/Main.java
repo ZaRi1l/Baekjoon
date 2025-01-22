cbbb1c1b30f616f2c1dfdc36a22471ab69f785a3
ZaRi1l <zari_09@naver.com>
2025-01-22 09:26:47 +0900

    q1654 try

--- Baekjoon/src/q1654/Main.java
+++ Baekjoon/src/q1654/Main.java
@@ -3,7 +3,6 @@ package q1654;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,37 +15,32 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 
 		int[] arr = new int[k];
-		int[] arr2;
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		Arrays.sort(arr);
-		arr2 = arr.clone();
-
-		int idx = arr2.length - 1;
 		int cm = 0;
-		while (true) {
-			int count = 0;
+		int max = 0;
+		for (int idx = arr.length - 1; idx >= 0; idx--) {
 
-			cm = arr2[idx];
-			for (int i = 0; i < arr.length; i++) {
-				count += arr[i] / cm;
+			int a = 1;
+			while (true) {
+				int count = 0;
+
+				cm = arr[idx] / a;
+				for (int i = 0; i < arr.length; i++) {
+					count += arr[i] / cm;
+				}
+
+				if (count >= n)
+					break;
+				a += 1;
 			}
-
-			if (count >= n)
-				break;
-
-			arr2[idx] = arr2[idx] / 2;
-			if (idx == 0) {
-				idx = arr2.length - 1;
-			} else {
-				idx -= 1;
-			}
+			max = (max < cm) ? cm : max;
 		}
 
-		System.out.println(cm);
+		System.out.println(max);
 
 //		for(int i = 0 ; i < arr.length; i++) {
 //			System.out.println(arr[i]);
