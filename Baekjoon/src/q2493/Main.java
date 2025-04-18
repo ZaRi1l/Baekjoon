1af277b2277e4886b584da81d47102bd9425e449
ZaRi <zari_09@naver.com>
2025-04-18 14:25:16 +0900

    q2493 explain commit

--- Baekjoon/src/q2493/Main.java
+++ Baekjoon/src/q2493/Main.java
@@ -13,23 +13,23 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		br.close();
 
-		int[] tops = new int[n];
-		int[] r = new int[n];
+		int[] tops = new int[n];	// 탑 크기
+		int[] r = new int[n];		// 레이저가 닿는 탑 번호
 		for (int i = 0; i < tops.length; i++) {
 			tops[i] = Integer.parseInt(st.nextToken());
 
 			for (int j = i - 1; j >= 0; j--) {
 				if (tops[j] >= tops[i]) {
-					r[i] = j + 1;
+					r[i] = j + 1;		// 나보다 큰 탑 번호 넣기
 					break;
 				}
 				
-				if (j != 0)
-					j = r[j];
+				// 해당 탑 보다 큰 곳으로 바로 이동
+				j = r[j];
 			}
 		}
 
-		StringBuffer sb = new StringBuffer();
+		StringBuffer sb = new StringBuffer();	// 출력
 		for (int i : r) {
 			sb.append(i).append(' ');
 		}
